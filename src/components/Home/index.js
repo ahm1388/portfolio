c38bb93b9d80a99e41e6aaa9c7b4c8fb87cafa89
Ahmed Al-Sabounchi <a_m_96@live.com>
2022-09-09 09:22:00 -0400

    Created contact form with email functionality

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters/index'
+import Loader from 'react-loaders'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -15,22 +16,25 @@ const Home = () => {
       }, [])
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                    <span className={letterClass}>H</span>
-                    <span className={`${letterClass} _12`}>i,</span>
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                        <br/>
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
                     <br/>
-                    <span className={`${letterClass} _13`}>I</span>
-                    <span className={`${letterClass} _14`}>'m</span>
-                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
-                <br/>
-                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={21} />
-                </h1>
-                <h2>Full-Stack Developer</h2>
-                <Link className="flat-button" to="/contact">CONTACT ME</Link>
+                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={21} />
+                    </h1>
+                    <h2>Full-Stack Developer</h2>
+                    <Link className="flat-button" to="/contact">CONTACT ME</Link>
+                </div>
             </div>
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 }
 
